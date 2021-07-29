7c9d858fada075b317df2da888ce74f2e9044355
luca-vogels <luporlp.yt@gmail.com>
2021-07-29 11:49:09 +0200

    LFU and LRU caches fully implemented and tested

--- README.md
+++ README.md
@@ -14,11 +14,23 @@ Uses the LFU algortihm (least frequently used) to evict entries if cache is full
 Additionally an expire interval can be set (passive on put call) that repeatedly halfs  
 the access counter of each entry.  
 
-#### `FixedEntryLFUCache`
+#### `FixedCountLFUCache`
 Cache storing fixed amount of key-value pairs.  
 Uses the LFU algortihm (least frequently used) to evict entries if cache is full.  
 Additionally an expire interval can be set (passive on put call) that repeatedly halfs  
 the access counter of each entry.  
+
+## Available Classes
+#### `FixedSizeLRUCache`
+Fixed size cache storing key-value pairs such that total 
+byte size of stored values will not exceed given limit. 
+Uses the LRU algortihm (least recently used) to evict entries if cache is full. 
+Cheaper and faster than LFU but not as many cache hits.  
+
+#### `FixedCountLRUCache`
+Cache storing fixed amount of key-value pairs.
+Uses the LRU algortihm (least recently used) to evict entries if cache is full. 
+Cheaper and faster than LFU but not as many cache hits.  
 
 
 ## Examples
--- index.js
+++ index.js
@@ -76,6 +76,7 @@ class FixedSizeLFUCache {
      *  - size:     int         Size in bytes of value (if value is {@type String} or {@type Buffer} size parameter not required)
      *  - onEvict:  Function    Callback that gets called with key and value that were evicted from cache (if returns {@code false} pair does not get evicted)
      * @returns True if successfully put key-value pair into cache or false if value is too big to fit into cache
+     * @throws Error if size is not given while value is not of type String nor of type Buffer
      */
     put(key, value, options={size: undefined, onEvict: function(k,v){}}){
         if(!key || (typeof key !== 'string' && !(key instanceof String))) throw new Error("Key must be defined and of type String");
@@ -184,8 +185,9 @@ class FixedSizeLFUCache {
  * the access counter of each entry.
  * @author LupCode.com
  */
-class FixedEntryLFUCache {
+class FixedCountLFUCache {
     #maxCount;
+    #count = 0;
     #expireMs;
     #entries = {}; // key: {value: Object, accesses: int, onEvict: function}
     #nextExpire;
@@ -205,7 +207,7 @@ class FixedEntryLFUCache {
      * @returns Amount of key-value pairs stored in cache
      */
     getCount(){
-        return Object.keys(this.#entries).length;
+        return this.#count;
     }
 
     /**
@@ -259,15 +261,16 @@ class FixedEntryLFUCache {
         }
 
         // evict entries until enough space is available for new entry
-        if(this.#maxCount >= this.getCount()){
+        if(this.#count >= this.#maxCount){
             let sorted = Object.entries(this.#entries).sort(function([, a], [, b]){ return a.accesses-b.accesses;  });
             let index = 0;
-            while(index < sorted.length && this.#maxCount >= this.getCount()) {
+            while(index < sorted.length && this.#count >= this.#maxCount) {
                 let entry = sorted[index++];
                 if(entry[1].onEvict && entry[1].onEvict(entry[0], entry[1].value) === false) continue;
+                this.#count--;
                 delete this.#entries[entry[0]];
             }
-            if(this.#maxCount >= this.getCount()) return false;
+            if(this.#count >= this.#maxCount) return false;
         }
 
         // put new entry into cache
@@ -276,7 +279,7 @@ class FixedEntryLFUCache {
             accesses: 1,
             onEvict: options.onEvict
         };
-
+        this.#count++;
         return true;
     }
 
@@ -304,6 +307,7 @@ class FixedEntryLFUCache {
         let entry = this.#entries[key];
         if(!entry) return undefined;
 
+        this.#count--;
         let value = entry.value;
         delete this.#entries[key];
         return value;
@@ -392,6 +396,7 @@ class FixedSizeLRUCache {
      *  - size:     int         Size in bytes of value (if value is {@type String} or {@type Buffer} size parameter not required)
      *  - onEvict:  Function    Callback that gets called with key and value that were evicted from cache (if returns {@code false} pair does not get evicted)
      * @returns True if successfully put key-value pair into cache or false if value is too big to fit into cache
+     * @throws Error if size is not given while value is not of type String nor of type Buffer
      */
     put(key, value, options={size: undefined, onEvict: function(k,v){}}){
         if(!key || (typeof key !== 'string' && !(key instanceof String))) throw new Error("Key must be defined and of type String");
@@ -415,6 +420,7 @@ class FixedSizeLRUCache {
                 if(entry.next) this.#entries[entry.next].prev = entry.prev; else this.#tail = entry.prev;
                 let nextKey = entry.next;
                 delete this.#entries[curr];
+                if(curr == this.#head) this.#head = nextKey;
                 curr = nextKey;
             }
             if(this.#maxSize - this.#size < options.size) return false;
@@ -431,7 +437,7 @@ class FixedSizeLRUCache {
         };
         if(!this.#head) this.#head = key;
         if(this.#tail) this.#entries[this.#tail].next = key;
-
+        this.#tail = key;
         return true;
     }
 
@@ -444,9 +450,13 @@ class FixedSizeLRUCache {
     get(key){
         let entry = this.#entries[key];
         if(!entry) return undefined;
-        
-        // TODO move to tail
-
+        if(key == this.#tail) return entry.value;
+        if(entry.prev) this.#entries[entry.prev].next = entry.next; else this.#head = entry.next;
+        if(entry.next) this.#entries[entry.next].prev = entry.prev; else this.#tail = entry.prev;
+        entry.next = null;
+        entry.prev = this.#tail;
+        if(this.#tail) this.#entries[this.#tail].next = key;
+        this.#tail = key;
         return entry.value;
     }
 
@@ -492,14 +502,156 @@ class FixedSizeLRUCache {
 }
 
 
-class FixedEntryLRUCache {
 
+
+/**
+ * Cache storing fixed amount of key-value pairs.
+ * Uses the LRU algortihm (least recently used) to evict entries if cache is full. 
+ * Cheaper and faster than LFU but not as many cache hits.
+ * @author LupCode.com
+ */
+class FixedCountLRUCache {
+    #maxCount;
+    #count = 0;
+    #head = null; // string
+    #tail = null; // string
+    #entries = {}; // key: {value: Object, size: int, prev: String, next: String, onEvict: function}
+
+    /**
+     * Creates a new cache with a fixed storing capacity
+     * @param {Number} maxCount Maximum amount of key-value pairs that can be hold
+     */
+    constructor(maxCount){
+        this.#maxCount = parseInt(maxCount);
+    }
+
+    /**
+     * @returns Amount of key-value pairs stored in cache
+     */
+     getCount(){
+        return this.#count;
+    }
+
+    /**
+     * @returns Maximum amount of key-value pairs that can be hold
+     */
+    getMaxCount(){
+        return this.#maxCount;
+    }
+
+    /**
+     * Sets the maximum amount of entries the cache can hold
+     * @param {int} maxCount Maximum amount of key-value pairs
+     */
+    setMaxCount(maxCount){
+        this.#maxCount = parseInt(maxCount);
+    }
+
+    /**
+     * Puts a key-value pair into cache
+     * @param {String} key Unique key to reference value in cache
+     * @param {*} value Value that should be put into cache
+     * @param {Object} options Additional options that can be set: 
+     *  - onEvict:  Function    Callback that gets called with key and value that were evicted from cache (if returns {@code false} pair does not get evicted)
+     * @returns True if successfully put key-value pair into cache or false if value is too big to fit into cache
+     * @throws Error if size is not given while value is not of type String nor of type Buffer
+     */
+    put(key, value, options={onEvict: function(k,v){}}){
+        if(!key || (typeof key !== 'string' && !(key instanceof String))) throw new Error("Key must be defined and of type String");
+        // evict entries until enough space is available for new entry
+        if(this.#count >= this.#maxCount){
+            let curr = this.#head;
+            while(curr && this.#count >= this.#maxCount) {
+                let entry = this.#entries[curr];
+                if(entry.onEvict && entry.onEvict(this.#head, entry.value) === false){ curr = entry.next; continue; }
+                if(entry.prev) this.#entries[entry.prev].next = entry.next; else this.#head = entry.next;
+                if(entry.next) this.#entries[entry.next].prev = entry.prev; else this.#tail = entry.prev;
+                this.#count--;
+                let nextKey = entry.next;
+                delete this.#entries[curr];
+                if(curr == this.#head) this.#head = nextKey;
+                curr = nextKey;
+            }
+            if(this.#count >= this.#maxCount) return false;
+        }
+
+        // put new entry into cache
+        this.#entries[key] = {
+            value: value,
+            prev: this.#tail,
+            next: null,
+            onEvict: options.onEvict
+        };
+        this.#count++;
+        if(!this.#head) this.#head = key;
+        if(this.#tail) this.#entries[this.#tail].next = key;
+        this.#tail = key;
+        return true;
+    }
+
+
+    /**
+     * Returns a value from the cache by its key (if it is still in the cache)
+     * @param {String} key Key of the value that should be returned
+     * @returns Value if found or undefined if not found
+     */
+    get(key){
+        let entry = this.#entries[key];
+        if(!entry) return undefined;
+        if(key == this.#tail) return entry.value;
+        if(entry.prev) this.#entries[entry.prev].next = entry.next; else this.#head = entry.next;
+        if(entry.next) this.#entries[entry.next].prev = entry.prev; else this.#tail = entry.prev;
+        entry.next = null;
+        entry.prev = this.#tail;
+        if(this.#tail) this.#entries[this.#tail].next = key;
+        this.#tail = key;
+        return entry.value;
+    }
+
+
+    /**
+     * Removes a key-value pair from the cache immediatly. 
+     * Will not trigger the onEvict callback if defined.
+     * @param {String} key Key of the value that should be removed
+     * @returns Value of the removed key-value pair or undefined if not found
+     */
+    remove(key){
+        let entry = this.#entries[key];
+        if(!entry) return undefined;
+        if(entry.prev) this.#entries[entry.prev].next = entry.next; else this.#head = entry.next;
+        if(entry.next) this.#entries[entry.next].prev = entry.prev; else this.#tail = entry.prev;
+
+        this.#count--;
+        let value = entry.value;
+        delete this.#entries[key];
+        return value;
+    }
+
+
+    /**
+     * Clears the cache without calling the {@code onEvict} callbacks (by default)
+     * @param {bool} callEvictCallbacks If {@code true} for each entry the {@code onEvict} callback will be called 
+     * which can prevent evicting the entry by returning {@type false}.
+     */
+    clear(callEvictCallbacks=false){
+        for(let entry of Object.entries(this.#entries)){
+            if(callEvictCallbacks && entry[1].onEvict && entry[1].onEvict(entry[0], entry[1].value) === false) continue;
+            this.remove(entry[0]);
+        }
+    }
+
+    /**
+     * @returns String containing basic information about this cache
+     */
+     toString(){
+        return this.constructor.name + "{entries=" + this.getCount() + "/" + this.getMaxCount() + "}";
+    }
 }
 
 
 module.exports = {
     FixedSizeLFUCache,
-    FixedEntryLFUCache,
+    FixedCountLFUCache,
     FixedSizeLRUCache,
-    FixedEntryLRUCache
+    FixedCountLRUCache
 }
